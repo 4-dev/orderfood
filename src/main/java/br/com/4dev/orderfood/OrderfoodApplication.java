d0aeddfc2713b80d0afcbc6b686dad4bbb68e06f
deyvid <Samsung@User>
2017-04-29 11:26:42 -0300

    commit da estrutura do projeto

---
+++ src/main/java/br/com/4dev/orderfood/OrderfoodApplication.java
@@ -0,0 +1,12 @@
+package br.com.4dev.orderfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderfoodApplication.class, args);
+	}
+}
