8fc299ee545caf7e48c5948a03fc27ee35450ff3
Marco Antônio <marco-oliveira@live.com>
2017-05-09 10:23:31 -0300

    configurando página de cadastro de produto

--- src/main/java/br/com/fourdev/orderfood/controller/ProdutoController.java
+++ src/main/java/br/com/fourdev/orderfood/controller/ProdutoController.java
@@ -6,34 +6,42 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.fourdev.orderfood.model.Produto;
 import br.com.fourdev.orderfood.service.ProdutoService;
 
 @RestController
-@RequestMapping("/produto")
 public class ProdutoController {
 
 	final static Logger logger = LoggerFactory.getLogger(ProdutoController.class);
 	@Autowired
 	private ProdutoService produtoService;
 
-	@RequestMapping(value = "/listarProdutos", method = RequestMethod.GET)
+	@RequestMapping(value = "produto/listarProdutos", method = RequestMethod.GET)
 	public List<Produto> selectProdutoList() {
 		return produtoService.selectProdutoList();
 	}
 
-	@RequestMapping(value = "/buscarProdutoPorCodigo/{id}", method = RequestMethod.GET)
+	@RequestMapping(value = "produto/buscarProdutoPorCodigo/{id}", method = RequestMethod.GET)
 	public Produto selectProdutoById(@PathVariable("id") String id) {
 		return produtoService.selectProdutoById(id);
 	}
-
-	@RequestMapping(value = "/savarProduto", method = RequestMethod.POST)
+	
+	@GetMapping("/novo")
+	public ModelAndView novo() {
+		ModelAndView modelAndView = new ModelAndView("produto/cadastro-produto");
+		return modelAndView;
+	}
+	
+	@PostMapping("produto/novo")
 	public void insertProduto(@RequestBody Produto produto) {
 
 		logger.debug("id=" + produto.getId());
@@ -43,13 +51,13 @@ public class ProdutoController {
 		produtoService.insertProduto(produto);
 	}
 
-	@RequestMapping(value = "/atualizarProduto/{id}", method = RequestMethod.PUT)
+	@RequestMapping(value = "produto/atualizarProduto/{id}", method = RequestMethod.PUT)
 	public void updateProduto(@RequestBody Produto produto, @PathVariable("id") String id) {
 		logger.debug("id=" + id);
 		produtoService.updateProduto(id, produto);
 	}
 
-	@RequestMapping(value = "/deletarProduto/{id}", method = RequestMethod.DELETE)
+	@RequestMapping(value = "produto/deletarProduto/{id}", method = RequestMethod.DELETE)
 	public void deleteProduto(@PathVariable("id") String id) {
 		produtoService.deleteProduto(id);
 	}
