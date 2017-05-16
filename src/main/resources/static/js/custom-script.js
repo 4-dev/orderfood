033c89afc58bb24e93c00693cc69c272c46fd809
Marco Antônio <marco-oliveira@live.com>
2017-05-16 20:09:29 -0300

    Implementando validaçoes e mascaras usando javascript no cadastro de produtos

--- src/main/resources/static/js/custom-script.js
+++ src/main/resources/static/js/custom-script.js
@@ -1,1 +1,11 @@
-﻿
+﻿$(function() {
+	var decimal = $('.js-decimal');
+	decimal.maskMoney({ prefix:'R$ ', allowNegative: true, decimal: ',', thousands: '.', affixesStay: false });
+	
+	
+	var plain = $('.js-plain');
+	plain.maskMoney({ precision: 0, thousands: '.' });
+	
+	var double = $('.js-double');
+	double.maskMoney({ precision: 3, decimal: ',', allowZero:true });
+});
