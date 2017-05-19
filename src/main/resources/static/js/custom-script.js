05c3badec080547535e2c637549e9e9bb226c65d
Marco Antônio <marco-oliveira@live.com>
2017-05-19 19:49:10 -0300

    Organização dos scripts no layout

--- src/main/resources/static/js/custom-script.js
+++ src/main/resources/static/js/custom-script.js
@@ -13,5 +13,4 @@ $(function() {
 	double.maskMoney({ precision: 3, allowZero:true, decimal: ',' });
 });
 
-//fechar alertas automatico
-$(".alert-dismissible").fadeTo(7000, 500).slideUp(500, function(){});
+
