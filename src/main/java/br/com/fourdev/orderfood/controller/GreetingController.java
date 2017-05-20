b7154a3bb9dc1d45ca80ca6c8813585a27de2c78
deyvid <Samsung@User>
2017-05-20 10:21:35 -0300

    Implementação do websocket

---
+++ src/main/java/br/com/fourdev/orderfood/controller/GreetingController.java
@@ -0,0 +1,21 @@
+package br.com.fourdev.orderfood.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+import br.com.fourdev.orderfood.model.Greeting;
+import br.com.fourdev.orderfood.model.HelloMessage;
+
+@Controller
+public class GreetingController {
+
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(180); // simulated delay
+        return new Greeting("Hello, " + message.getName() + "!");
+    }
+
+}
