dde9becd427740a2d5e0f7dcaaac8b0ed95d5a78
deyvid <Samsung@User>
2017-05-29 00:14:10 -0300

    - Ajuste da lista de pedido - Adicionado o cliente, a mesa e o usuário no cabeçalho do pedido

---
+++ src/main/java/br/com/fourdev/orderfood/model/Cliente.java
@@ -0,0 +1,67 @@
+package br.com.fourdev.orderfood.model;
+
+public class Cliente {
+
+	private int idcliente;
+	private String nome;
+	private String sexo;
+
+	public int getIdcliente() {
+		return idcliente;
+	}
+
+	public void setIdcliente(int idcliente) {
+		this.idcliente = idcliente;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + idcliente;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((sexo == null) ? 0 : sexo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		if (idcliente != other.idcliente)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (sexo == null) {
+			if (other.sexo != null)
+				return false;
+		} else if (!sexo.equals(other.sexo))
+			return false;
+		return true;
+	}
+
+}
--- src/main/java/br/com/fourdev/orderfood/model/Pedido.java
+++ src/main/java/br/com/fourdev/orderfood/model/Pedido.java
@@ -1,83 +1,129 @@
 package br.com.fourdev.orderfood.model;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Calendar;
 import java.util.List;
 
 public class Pedido {
-	
-	private long numped; //id
-	private List<ItemPedido> itens; 
-	private LocalDateTime dataCriacao; 
-	private BigDecimal valorDesconto; //id
-	private BigDecimal valorTotal; //id		
-	private String observacao; //id
+
+	private long numped; // id
+	private List<ItemPedido> itens;
+	private LocalDateTime dataCriacao;
+	private BigDecimal valorDesconto; // id
+	private BigDecimal valorTotal; // id
+	private String observacao; // id
 	private LocalDateTime dataEntrega;
 	private StatusPedido status;
+	private Cliente cliente;
+	private Mesa mesa;
+	private Usuario usuario;
+
 	public long getNumped() {
 		return numped;
 	}
+
 	public void setNumped(long numped) {
 		this.numped = numped;
 	}
+
 	public List<ItemPedido> getItens() {
 		return itens;
 	}
+
 	public void setItens(List<ItemPedido> itens) {
 		this.itens = itens;
 	}
+
 	public LocalDateTime getDataCriacao() {
 		return dataCriacao;
 	}
+
 	public void setDataCriacao(LocalDateTime dataCriacao) {
 		this.dataCriacao = dataCriacao;
 	}
+
 	public BigDecimal getValorDesconto() {
 		return valorDesconto;
 	}
+
 	public void setValorDesconto(BigDecimal valorDesconto) {
 		this.valorDesconto = valorDesconto;
 	}
+
 	public BigDecimal getValorTotal() {
 		return valorTotal;
 	}
+
 	public void setValorTotal(BigDecimal valorTotal) {
 		this.valorTotal = valorTotal;
 	}
+
 	public String getObservacao() {
 		return observacao;
 	}
+
 	public void setObservacao(String observacao) {
 		this.observacao = observacao;
 	}
+
 	public LocalDateTime getDataEntrega() {
 		return dataEntrega;
 	}
+
 	public void setDataEntrega(LocalDateTime dataEntrega) {
 		this.dataEntrega = dataEntrega;
 	}
+
 	public StatusPedido getStatus() {
 		return status;
 	}
+
 	public void setStatus(StatusPedido status) {
 		this.status = status;
 	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Mesa getMesa() {
+		return mesa;
+	}
+
+	public void setMesa(Mesa mesa) {
+		this.mesa = mesa;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((cliente == null) ? 0 : cliente.hashCode());
 		result = prime * result + ((dataCriacao == null) ? 0 : dataCriacao.hashCode());
 		result = prime * result + ((dataEntrega == null) ? 0 : dataEntrega.hashCode());
 		result = prime * result + ((itens == null) ? 0 : itens.hashCode());
+		result = prime * result + ((mesa == null) ? 0 : mesa.hashCode());
 		result = prime * result + (int) (numped ^ (numped >>> 32));
 		result = prime * result + ((observacao == null) ? 0 : observacao.hashCode());
 		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result + ((usuario == null) ? 0 : usuario.hashCode());
 		result = prime * result + ((valorDesconto == null) ? 0 : valorDesconto.hashCode());
 		result = prime * result + ((valorTotal == null) ? 0 : valorTotal.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -87,6 +133,11 @@ public class Pedido {
 		if (getClass() != obj.getClass())
 			return false;
 		Pedido other = (Pedido) obj;
+		if (cliente == null) {
+			if (other.cliente != null)
+				return false;
+		} else if (!cliente.equals(other.cliente))
+			return false;
 		if (dataCriacao == null) {
 			if (other.dataCriacao != null)
 				return false;
@@ -102,6 +153,11 @@ public class Pedido {
 				return false;
 		} else if (!itens.equals(other.itens))
 			return false;
+		if (mesa == null) {
+			if (other.mesa != null)
+				return false;
+		} else if (!mesa.equals(other.mesa))
+			return false;
 		if (numped != other.numped)
 			return false;
 		if (observacao == null) {
@@ -110,6 +166,11 @@ public class Pedido {
 		} else if (!observacao.equals(other.observacao))
 			return false;
 		if (status != other.status)
+			return false;
+		if (usuario == null) {
+			if (other.usuario != null)
+				return false;
+		} else if (!usuario.equals(other.usuario))
 			return false;
 		if (valorDesconto == null) {
 			if (other.valorDesconto != null)
@@ -123,6 +184,5 @@ public class Pedido {
 			return false;
 		return true;
 	}
-	
-	
+
 }
