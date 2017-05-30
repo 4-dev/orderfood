6b4f140adc53ee86bbf9eb2ccc78841cb5192db8
deyvid <Samsung@User>
2017-05-30 00:38:53 -0300

    - ajustado statusmesa; - criado repository do cliente

--- src/main/java/br/com/fourdev/orderfood/repository/pedido/PedidoRowMapper.java
+++ src/main/java/br/com/fourdev/orderfood/repository/pedido/PedidoRowMapper.java
@@ -3,16 +3,28 @@ package br.com.fourdev.orderfood.repository.pedido;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import br.com.fourdev.orderfood.model.Cliente;
 import br.com.fourdev.orderfood.model.Mesa;
 import br.com.fourdev.orderfood.model.Pedido;
 import br.com.fourdev.orderfood.model.Usuario;
+import br.com.fourdev.orderfood.repository.cliente.ClienteRepository;
+import br.com.fourdev.orderfood.repository.cliente.ClienteRepositoryImpl;
+import br.com.fourdev.orderfood.repository.mesa.MesaRepository;
+import br.com.fourdev.orderfood.repository.mesa.MesaRepositoryImpl;
 
+@Service
 public class PedidoRowMapper implements RowMapper<Pedido> {
+	@Autowired
+	ClienteRepository clienteRepository;
+
+	@Autowired
+	MesaRepository mesaRepository;
 
 	@Override
 	public Pedido mapRow(ResultSet rs, int rowNum) throws SQLException {
@@ -24,15 +36,19 @@ public class PedidoRowMapper implements RowMapper<Pedido> {
 		pedido.setValorDesconto(rs.getBigDecimal("valordesconto"));
 		pedido.setValorTotal(rs.getBigDecimal("valortotal"));
 		pedido.setObservacao(rs.getString("observacao"));
-//		pedido.setDataEntrega(LocalDateTime.parse(rs.getString("dataentrega"), DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-//		pedido.setDataCancel(LocalDateTime.parse(rs.getString("datacancel"), DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+		// pedido.setDataEntrega(LocalDateTime.parse(rs.getString("dataentrega"),
+		// DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+		// pedido.setDataCancel(LocalDateTime.parse(rs.getString("datacancel"),
+		// DateTimeFormatter.ISO_LOCAL_DATE_TIME));
 		// pedido.setStatus();
 		Cliente cliente = new Cliente();
 		cliente.setIdcliente(rs.getInt("idcliente"));
+//		cliente = clienteRepository.selectClientePorId(rs.getInt("idcliente"));
 		pedido.setCliente(cliente);
 
 		Mesa mesa = new Mesa();
 		mesa.setIdmesa(rs.getInt("idmesa"));
+//		mesa = mesaRepository.selectMesaPorId(rs.getInt("idmesa"));
 		pedido.setMesa(mesa);
 
 		Usuario usuario = new Usuario();
