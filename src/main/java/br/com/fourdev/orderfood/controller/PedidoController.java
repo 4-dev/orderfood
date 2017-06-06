46d2611ce1b47d8fc55cac78355e3426983e3ccb
deyvid <Samsung@User>
2017-06-05 22:47:22 -0300

    melhorando o cadastro de pedido

--- src/main/java/br/com/fourdev/orderfood/controller/PedidoController.java
+++ src/main/java/br/com/fourdev/orderfood/controller/PedidoController.java
@@ -1,5 +1,8 @@
 package br.com.fourdev.orderfood.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import br.com.fourdev.orderfood.model.ItemPedido;
 import br.com.fourdev.orderfood.model.Pedido;
+import br.com.fourdev.orderfood.service.MesaService;
 import br.com.fourdev.orderfood.service.PedidoService;
 
 @Controller
@@ -20,6 +25,12 @@ public class PedidoController {
 	
 	@Autowired
 	private PedidoService pedidoService;
+	@Autowired
+	private MesaService mesaService;
+	
+	private Pedido pedido;
+	private List<ItemPedido> itens = new ArrayList<ItemPedido>();
+	
 	public boolean listaEmAndamento;
 	
 	
@@ -55,19 +66,30 @@ public class PedidoController {
 		return modelAndView ;
 	}	
 
+
+	@PostMapping("/additem")
+	public ModelAndView additem(@Valid ItemPedido itemPedido, BindingResult result, RedirectAttributes attributes){
+		itens.add(itemPedido);
+//		pedidoService.insertMesa(pedido);
+		attributes.addFlashAttribute("mensagem", "Mesa Salva com Sucesso!");
+		
+		return new ModelAndView("redirect:novo");
+		
+	}
+
 	
 	@GetMapping("/novo")
-	public ModelAndView novo(Pedido pedido){
+	public ModelAndView novo(Pedido pedido, ItemPedido itemPedido){
 		ModelAndView modelAndView = new ModelAndView("pedido/cadastro-pedido");
 		modelAndView.addObject(pedido);
-		
+		modelAndView.addObject(itens);
 		return modelAndView ;
 	}
 	
 	@PostMapping("/novo")
 	public ModelAndView novo(@Valid Pedido pedido, BindingResult result, RedirectAttributes attributes){
 		if (result.hasErrors()) {
-			return novo(pedido);
+			return novo(pedido, null);
 		}
 //		pedidoService.insertMesa(pedido);
 		attributes.addFlashAttribute("mensagem", "Mesa Salva com Sucesso!");
