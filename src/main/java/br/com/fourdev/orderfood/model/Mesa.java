24adb6378c853ea1f71f7f42963393d69b390926
deyvid <Samsung@User>
2017-06-22 01:40:16 -0300

    - ajuste para comunicação com o app celular

--- src/main/java/br/com/fourdev/orderfood/model/Mesa.java
+++ src/main/java/br/com/fourdev/orderfood/model/Mesa.java
@@ -9,7 +9,6 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Transient;
-import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotBlank;
 
