590391d5c17f7cad42b8a60479f9bd4b62764e05
deyvid <Samsung@User>
2017-06-22 01:42:00 -0300

    - Ajuste para comunicação com app mobile

--- src/main/java/br/com/fourdev/orderfood/controller/apirest/MesaRestController.java
+++ src/main/java/br/com/fourdev/orderfood/controller/apirest/MesaRestController.java
@@ -1,20 +1,24 @@
 
 package br.com.fourdev.orderfood.controller.apirest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
+
 import br.com.fourdev.orderfood.model.Mesa;
 import br.com.fourdev.orderfood.service.MesaService;
-import br.com.fourdev.orderfood.service.PedidoService;
 
 //@ImportResource("classpath:org/springframework/integration/samples/chat/stomp/server/stomp-server.xml")
 @RestController
@@ -24,7 +28,7 @@ public class MesaRestController {
 	final static Logger logger = LoggerFactory.getLogger(MesaRestController.class);
 	@Autowired
 	private MesaService mesaService;
-	
+
 	// private SimpMessagingTemplate template;
 
 	@RequestMapping(value = "/listarMesas", method = RequestMethod.GET)
@@ -58,9 +62,25 @@ public class MesaRestController {
 		mesaService.deleteMesa(id);
 	}
 
-	@RequestMapping(value = "/verificarmesa/{id}", method = RequestMethod.GET)
-	public boolean verificarStatusMesa(@PathVariable("id") int idmesa) throws Exception {
-		return mesaService.verificarStatusMesa(idmesa);
-	}
+	@RequestMapping(value = "/verificarmesa/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public String  verificarStatusMesa(@PathVariable("id") int idmesa) throws Exception {
+//		Mesa mesa = new Mesa();
+//		mesa.setIdmesa(123);
+//		mesa.setDescricao("João da Silva");
+//		mesa.setStatus("joaodasilva@mail.com");
 
+		// código que faz o trabalho ;-)
+		Gson gson = new Gson();
+		String valor = "";
+		if (mesaService.verificarStatusMesa(idmesa)) {
+			valor += "Mesa disponivel";
+		} else {
+			valor += "Mesa ocupada";
+		}
+		String userJSONString = gson.toJson(valor);
+
+		valor = gson.fromJson(userJSONString, String.class);
+		return valor;
+
+	}
 }
