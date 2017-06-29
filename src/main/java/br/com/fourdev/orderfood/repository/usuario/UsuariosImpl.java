6868bba4686edd6d7264cffbe65a57cfd7b65e95
deyvid <Samsung@User>
2017-06-29 03:12:14 -0300

    ajuste para adicionar dinamicamente o schema padrão: "set search_path to orderfood"

--- src/main/java/br/com/fourdev/orderfood/repository/usuario/UsuariosImpl.java
+++ src/main/java/br/com/fourdev/orderfood/repository/usuario/UsuariosImpl.java
@@ -3,36 +3,44 @@ package br.com.fourdev.orderfood.repository.usuario;
 import java.util.List;
 import java.util.Optional;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import br.com.fourdev.orderfood.model.Usuario;
 
+@Repository("usuariosImpl")
 public class UsuariosImpl implements UsuariosQueries {
 
 	@Autowired
 	private EntityManager manager;
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public Optional<Usuario> porEmailEAtivo(String email) {
+		String query1 = "set search_path to orderfood, public";
+		jdbcTemplate.update(query1, new Object[]{});
+
+		
 		String query = "SELECT  * from usuario WHERE email LIKE lower(:email) AND ativo = true";
 
-		return manager
-				.createNativeQuery(query, Usuario.class)
-					.setParameter("email", email).getResultList()
-						.stream().findFirst();
+		return manager.createNativeQuery(query, Usuario.class).setParameter("email", email).getResultList().stream()
+				.findFirst();
 	}
-	
-	
-	//jpql
+
+	// jpql
 	@Override
 	public List<String> permissoes(Usuario usuario) {
 		return manager.createQuery(
-				"select distinct p.nome from Usuario u inner join u.grupos g inner join g.permissoes p where u = :usuario", String.class)
-				.setParameter("usuario", usuario)
-				.getResultList();
+				"select distinct p.nome from Usuario u inner join u.grupos g inner join g.permissoes p where u = :usuario",
+				String.class).setParameter("usuario", usuario).getResultList();
 	}
 
 }
