382d95d2c565a6a6d1e1c45bcbf54706e9bcf763
deyvid <Samsung@User>
2017-07-04 02:15:05 -0300

    - Ajustado json para o objeto Pedido; - Ajustado para inserir pedido nas tabelas.

--- src/main/java/br/com/fourdev/orderfood/service/PedidoService.java
+++ src/main/java/br/com/fourdev/orderfood/service/PedidoService.java
@@ -1,5 +1,8 @@
 package br.com.fourdev.orderfood.service;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -7,6 +10,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import br.com.fourdev.orderfood.model.Cliente;
+import br.com.fourdev.orderfood.model.ItemPedido;
 import br.com.fourdev.orderfood.model.Mesa;
 import br.com.fourdev.orderfood.model.Pedido;
 import br.com.fourdev.orderfood.model.StatusPedido;
@@ -24,6 +34,9 @@ public class PedidoService {
 		pedidoRepository.insertPedido(pedido);
 	}
 
+	public void insertPedido(List<Pedido> pedidos) {
+		pedidoRepository.insertPedido(pedidos);
+	}	
 
 	public List<Pedido> pedidosAbertos() {
 		StatusPedido statusPedido = StatusPedido.ABERTO;
@@ -49,6 +62,68 @@ public class PedidoService {
 	public List<Pedido> retornaPedidoPorMesa(int idmesa) {
 		return pedidoRepository.retornaPedidoPorMesa(idmesa);
 	}
+	
+	
+
+	private List<Pedido> transformarJsonPedido(String pedidoJson) {
+
+//		Gson gson = new Gson();
+		String jsonInString = pedidoJson;
+		JsonParser jsonParser = new JsonParser();
+		JsonObject jo = (JsonObject) jsonParser.parse(jsonInString);
+		JsonArray jsonArr = jo.getAsJsonArray("listPedidos");
+		List<Pedido> pedidos = new ArrayList<Pedido>();
+
+		for (int i = 0; i < jsonArr.size(); i++) {
+			System.out.println(jsonArr.get(i).getAsJsonObject().get("codigo"));
+			System.out.println(jsonArr.get(i).getAsJsonObject().get("dtEmissao").getAsString());
+
+			List<ItemPedido> itens = new ArrayList<ItemPedido>();
+			JsonArray jsonItens = jsonArr.get(i).getAsJsonObject().get("itens").getAsJsonArray();
+			for (int j = 0; j < jsonItens.size(); j++) {
+				int codPedido = jsonItens.get(i).getAsJsonObject().get("codPedido").getAsInt();
+				int codProduto = jsonItens.get(i).getAsJsonObject().get("codProduto").getAsInt();
+				int quantidade = jsonItens.get(i).getAsJsonObject().get("quantidade").getAsInt();
+				BigDecimal valorTotal = jsonItens.get(i).getAsJsonObject().get("valorTotal").getAsBigDecimal();
+				BigDecimal valorUnitario = jsonItens.get(i).getAsJsonObject().get("valorUnitario").getAsBigDecimal();
+
+				System.out.println(codPedido);
+				ItemPedido itemPedido = new ItemPedido();
+				itemPedido.setNumped(codPedido);
+				itemPedido.setProduto(codProduto);
+				itemPedido.setQuantidade(quantidade);
+				itemPedido.setValorUnitario(valorUnitario);
+				itens.add(itemPedido);
+
+				System.out.println(codProduto);
+				System.out.println(quantidade);
+				System.out.println(valorTotal);
+				System.out.println(valorUnitario);
+			}
+
+			Pedido pedido = new Pedido();
+			Cliente cliente = new Cliente();
+			cliente.setIdcliente(1);
+			cliente.setNome("admin");
+			pedido.setCliente(cliente);
+			pedido.setDataCriacao(LocalDateTime.now());
+			pedido.setDataEntrega(LocalDateTime.now());
+			pedido.setObservacao("");
+			pedido.setStatus(StatusPedido.ABERTO);
+
+			// pedido.setUsuario(usuarios.findOne((long) 1));
+			Mesa mesa = new Mesa();
+			mesa.setDescricao("");
+			pedido.setMesa(mesa);
+
+			pedido.setItens(itens);
+//			pedido.setNumped(jsonArr.get(i).getAsJsonObject().get("codigo").getAsInt());
+			pedidos.add(pedido);
+		}
+	
+		return pedidos;
+	}
+
 
 	// public StatusPedido reservarMesa() {
 
