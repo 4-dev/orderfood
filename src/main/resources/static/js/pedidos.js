27f03375591690c73381a469718d9f6a9f9df1d3
Marco Antônio <marco-oliveira@live.com>
2017-07-10 10:25:47 -0300

    Confirmação no botão finaliza mesa

--- src/main/resources/static/js/pedidos.js
+++ src/main/resources/static/js/pedidos.js
@@ -3,39 +3,70 @@ $('.js-itens-btn').click(function(evento){
 	event.preventDefault(); 
 	var botaoClicado = $(evento.currentTarget);
 	var urlItem = botaoClicado.data('url');
-            $("#jsGrid-odata").jsGrid({
-                height: "auto",
-                width: "100%",
-                sorting: true,
-                paging: false,
-                autoload: true,
-                controller: {
-                    loadData: function() {
-                        var deferred = $.Deferred();
-                        $.ajax({
-                            url: urlItem,
-                            dataType: "json",
-                            success: function(data){
-                            	console.log('Dados', data);
-                                deferred.resolve(data);
-                                console.log('deferred', deferred.resolve(data));
-                            }
-                        });
-         
-                        return deferred.promise();
+    $("#jsGrid-odata").jsGrid({
+        height: "auto",
+        width: "100%",
+        sorting: true,
+        paging: false,
+        autoload: true,
+        controller: {
+            loadData: function() {
+                var deferred = $.Deferred();
+                $.ajax({
+                    url: urlItem,
+                    dataType: "json",
+                    success: function(data){
+                    	console.log('Dados', data);
+                        deferred.resolve(data);
+                        console.log('deferred', deferred.resolve(data));
                     }
-                },
-                fields: [
-                    { name: "produto", type: "number", align: "center", width: 30 },
-                    { name: "nome", type: "text", width: 150, align: "center" },
-                    { name: "quantidade", type: "number", width: 50, align: "center" },
-                    { name: "valorUnitario", type: "number", width: 50, align: "center",
+                });
+ 
+                return deferred.promise();
+            }
+        },
+        fields: [
+            { name: "produto", type: "number", align: "center", width: 30 },
+            { name: "nome", type: "text", width: 150, align: "center" },
+            { name: "quantidade", type: "number", width: 50, align: "center" },
+            { name: "valorUnitario", type: "number", width: 50, align: "center",
+                itemTemplate: function(value) {
+                    return "R$ "+value.toFixed(2); } },
+            { name: "valorTotal", type: "number", width: 50, align: "center",
                         itemTemplate: function(value) {
-                            return "R$ "+value.toFixed(2); } },
-                    { name: "valorTotal", type: "number", width: 50, align: "center",
-                                itemTemplate: function(value) {
-                                    return "R$ "+value.toFixed(2); } }
-                ]
-            });
-         
+                            return "R$ "+value.toFixed(2); } }
+        ]
+    });
+ 
+});
+
+$('.js-confirma-btn').click(function(evento){
+	event.preventDefault(); 
+	var botaoClicado = $(evento.currentTarget);
+	var url = botaoClicado.data('url');
+	var objeto = botaoClicado.data('objeto');
+	swal({   title: "Finalizar "+objeto+"?",   
+        text: "Total: R$ ",   
+        type: "info",   showCancelButton: true,   
+        closeOnConfirm: false,   
+        showLoaderOnConfirm: true, }, 
+        function(){   
+        	$.ajax({
+    			url: url,
+    			method: 'get',
+    			success: onSucesso.bind(this),
+    			error: onErro.bind(this)
+    		});
+            function onSucesso(){
+        		console.log('Finalizada com sucesso!');
+        	}
+        	
+        	function onErro(e) {
+        		swal('Oops!', e.responseText, 'error');
+        	}
+            setTimeout(function(){     
+            	swal("Conta finalizada!");   
+            	}, 2000); 
         });
+});
+	
